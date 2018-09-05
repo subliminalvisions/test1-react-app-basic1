ed66d8823b561d80fc738521d935a1c765b0eb34
subliminalvisions <subliminalvisions@gmail.com>
2018-09-04 21:45:23 -0600

    made basic edits to App.js file to test code running

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+// edit file and save - testing
+// ooohhh i can just start editing the code right here ... yey ... 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <hr/>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to The Basic React Test App</h1>
+          <hr/>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started coding, edit the code in <code>src/App.js</code>, modify and save to reload.
         </p>
       </div>
     );
